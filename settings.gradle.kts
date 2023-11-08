56045535ecd3c84999d409e66d29fe849d4b72c9
xx <1263746648@qq.com>
2023-11-08 18:47:34 +0800

    opencv

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,7 @@ pluginManagement {
         google()
         mavenCentral()
         maven(uri("https://jitpack.io"))
+        maven { url = uri("https://chaquo.com/maven-test") }//为了使用chaquo15.0.0版本，是一个预发布版，更好的支持build.gradle.kts
     }
 }
 dependencyResolutionManagement {
@@ -12,9 +13,11 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven(uri("https://jitpack.io"))
+        maven { url = uri("https://chaquo.com/maven-test") }
     }
 }
 
 rootProject.name = "KHTDemo"
 include(":app")
- 
+include(":opencv")
+//include(":opencv_sdk")
